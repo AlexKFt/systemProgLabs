e0f1adf7cb00f90d7c82fc82a22bc1d73da73b6f
AlekanderKushch <vip.alex2132@mail.ru>
2024-02-27 14:27:38 +0300

    Add project files.

---
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -0,0 +1,105 @@
+#include "pch.h"
+#include "Session.h"
+#include "SafeWriting.h"
+
+
+
+
+DWORD WINAPI workFunc(LPVOID _param)
+{
+	auto session = static_cast<Session*>(_param);
+	SafeWrite("session", session->sessionID, "created");
+	while (true)
+	{
+		Message m;
+		if (session->getMessage(m))
+		{
+			switch (m.header.messageType)
+			{
+			case MT_CLOSE:
+			{
+				SafeWrite("session", session->sessionID, "closed");
+				delete session;
+				return 0;
+			}
+			case MT_DATA:
+			{
+				SafeWrite("session", session->sessionID, "data", m.data);
+				Sleep(100 * session->sessionID);
+				break;
+			}
+			}
+		}
+	}
+	return 0;
+}
+
+
+
+int main()
+{
+	HANDLE confirmEvent = ::CreateEvent(NULL, FALSE, FALSE, "ConfirmEvent");
+	HANDLE startEvent = ::CreateEvent(NULL, FALSE, FALSE, "StartEvent");
+	HANDLE stopEvent = ::CreateEvent(NULL, FALSE, FALSE, "CloseProcKushch");
+	HANDLE exitEvent = ::CreateEvent(NULL, FALSE, FALSE, "ExitProcKushch");
+
+
+	HANDLE events[3];
+	events[0] = exitEvent;
+	events[1] = stopEvent;
+	events[2] = startEvent;
+
+	::SetEvent(confirmEvent);
+
+	std::vector<HANDLE> hThreads;
+	std::vector<Session*> sessions;
+
+	while (true)
+	{
+		DWORD dwWaitRes = ::WaitForMultipleObjects(sizeof(events)/ sizeof(HANDLE), events, FALSE, INFINITE);
+		switch (dwWaitRes)
+		{
+			case WAIT_OBJECT_0 :
+				for (int i{ 0 }; i < sessions.size(); ++i)
+				{
+					sessions[i]->addMessage(MT_CLOSE);
+					CloseHandle(hThreads[i]);
+				}
+				return 0;
+			case WAIT_OBJECT_0 + 1:
+
+				if (!hThreads.size())
+				{
+					::SetEvent(confirmEvent);
+					return 0;
+				}	
+				sessions.back()->addMessage(MT_CLOSE);
+				sessions.pop_back();
+
+				::CloseHandle(hThreads.back());
+				hThreads.pop_back();
+
+				break;
+
+			case WAIT_OBJECT_0 + 2:
+
+				sessions.push_back(new Session(hThreads.size()));
+				hThreads.push_back(::CreateThread(NULL,0,workFunc, (LPVOID)sessions.back(), 0, NULL));
+				break;
+
+			default:
+				
+				std::cout << "There was an error" << std::endl;
+				return 0;
+		}
+		::SetEvent(confirmEvent);
+	}
+
+	WaitForMultipleObjects((DWORD)hThreads.size(), hThreads.data(), TRUE, INFINITE);
+
+	CloseHandle(confirmEvent);
+	CloseHandle(startEvent);
+	CloseHandle(stopEvent);
+
+	return 0;
+}
---
+++ systemProgLab1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace systemProgLab1
+{
+    public partial class Form1 : Form
+    {
+        Process childProcess = null;
+        EventWaitHandle eventStart = new EventWaitHandle(false, EventResetMode.AutoReset, "StartEvent");
+        EventWaitHandle eventConfirm = new EventWaitHandle(false, EventResetMode.AutoReset, "ConfirmEvent");
+        EventWaitHandle eventClose = new EventWaitHandle(false, EventResetMode.AutoReset, "CloseProcKushch");
+        EventWaitHandle eventExit = new EventWaitHandle(false, EventResetMode.AutoReset, "ExitProcKushch");
+
+        int threadsCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {  
+            if (childProcess==null || childProcess.HasExited)
+            {
+                threadsBox.Items.Clear();
+                childProcess = Process.Start("ConsoleApplication.exe");
+                eventConfirm.WaitOne();
+                updateList("main");
+                updateList("all threads");
+                threadsCounter++;
+
+            }
+            else
+            {
+                int n = 0;
+                try
+                {
+                    n = Int32.Parse(thredsPerCliccEdit.Text); 
+                }
+                catch 
+                { }
+ 
+                for (int i = 0; i < n; ++i)
+                {
+                    eventStart.Set();
+                    eventConfirm.WaitOne();
+                    updateList((threadsCounter++).ToString());
+                }         
+            }
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            if (childProcess==null || childProcess.HasExited)
+            {
+                threadsBox.Items.Clear();
+            }
+            else
+            {
+                eventClose.Set();
+                eventConfirm.WaitOne();
+                threadsBox.Items.RemoveAt(--threadsCounter);
+            }
+        }
+
+        private void updateList(string name)
+        { 
+            threadsBox.Items.Add(name);
+        }
+    }
+}
